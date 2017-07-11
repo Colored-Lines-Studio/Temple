cd50f391f934b3d067252c00b9fefcfdaab74248
atemitope <amodut@gmail.com>
2017-07-11 21:46:11 +0100

    refactor loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,8 +1,16 @@
 (function () {
   function setupPage() {
+    var loader = _('.loader'),
+        body = _('body'),
+        container = _('.body-container'),
+        audio = _('.bg-music audio');
+
     setTimeout(function () {
-      _('.loader').style.transform = 'translateX(-100%)';
-      _('.bg-music audio').play();
+      loader.style.transform = 'translateX(-100%)';
+      body.style.backgroundImage = 'url("assets/images/bg.jpg")';
+      container.style.display = 'flex';
+      audio.play();
+      audio.volume = 0.7;
     }, 1000);
   }
 
