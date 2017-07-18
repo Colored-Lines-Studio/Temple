c9f0b37b26fc5e917ef31371a09ddcccac827d6a
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-07-18 07:39:21 +0100

    Fix for responsive issues introduced through loader js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,16 +1,15 @@
 (function () {
   function setupPage() {
     var loader = _('.loader'),
-        body = _('body'),
-        container = _('.body-container'),
         audio = _('.bg-music audio');
 
     setTimeout(function () {
       loader.style.transform = 'translateX(-100%)';
-      body.style.backgroundImage = 'url("assets/images/bg.jpg")';
-      container.style.display = 'flex';
       audio.play();
       audio.volume = 0.7;
+
+      // Setup page intro transitions
+      new WOW().init();
     }, 1000);
   }
 
